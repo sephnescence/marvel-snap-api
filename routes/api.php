3ec452ef3fb5e03afeda8a8ed1b4f030268f4648
Blake Taylor <sephnescence@gmail.com>
2023-02-24 22:54:35 +1000

    make a test controller so that I don't have to actually hit snap fan

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SnapFanCards\SnapFanCards;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Testing a json response
+Route::get('/snap_fan_cards', [SnapFanCards::class, 'page']);
